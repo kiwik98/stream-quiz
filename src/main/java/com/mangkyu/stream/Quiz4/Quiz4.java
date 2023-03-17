70a5fb50ceb3d0de190178214a7ea7aed11ac898
kiwikmd <kiwik98@modeal.net>
2023-03-17 18:27:35 +0900

    문제4-3까지 풀이 완료

--- src/main/java/com/mangkyu/stream/Quiz4/Quiz4.java
+++ src/main/java/com/mangkyu/stream/Quiz4/Quiz4.java
@@ -3,7 +3,9 @@ package com.mangkyu.stream.Quiz4;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Quiz4 {
 
@@ -27,17 +29,29 @@ public class Quiz4 {
 
     // 4.1 2020년에 일어난 모든 거래 내역을 찾아 거래값을 기준으로 오름차순 정렬하라.
     public List<Transaction> quiz1() {
-        return Collections.emptyList();
+        return transactions.stream()
+                .filter(t -> t.getYear() == 2020)
+                .sorted(Comparator.comparing(Transaction::getValue))
+                .collect(Collectors.toList());
     }
 
     // 4.2 거래 내역이 있는 거래자가 근무하는 모든 도시를 중복 없이 나열하라.
     public List<String> quiz2() {
-        return Collections.emptyList();
+        return transactions.stream()
+                .map(t -> t.getTrader().getCity())
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     // 4.3 서울에서 근무하는 모든 거래자를 찾아서 이름순서대로 정렬하라.
     public List<Trader> quiz3() {
-        return Collections.emptyList();
+        return transactions.stream()
+                .map(t -> t.getTrader())
+                .distinct()
+                .filter(t -> t.getCity().equalsIgnoreCase("seoul"))
+                .sorted(Comparator.comparing(Trader::getName))
+                .collect(Collectors.toList());
+
     }
 
     // 4.4 모든 거래자의 이름을 구분자(",")로 구분하여 정렬하라.
